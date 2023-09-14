4f9b9c1d4b3a16293f62bdf3bf2d9c905aa4c3d8
Karthi Keyan <125793976+karthi200125@users.noreply.github.com>
2023-09-14 14:24:40 +0530

    home update

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import './Home.css'
 import Navbar from '../../components/Navbar/Navbar'
-import Header from '../../components/Header/header'
-import List from '../List/List'
 import Featured from '../../components/Featured/featured'
 import PropertyList from '../../components/PropertyList/PropertyList'
 import FeaturedProperties from '../../components/FeaturedProperties/FeaturedProperties'
 import MailList from '../../components/MailList/MailList'
 import Footer from '../../components/Footer/Footer'
+import Header from '../../components/Header/Header'
 
 
 const Home = () => {
