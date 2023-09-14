10e8ab2ac8db1897e6cbfc1f27396eda1d137e4e
Karthi Keyan <125793976+karthi200125@users.noreply.github.com>
2023-09-14 14:32:07 +0530

    update featured

--- src/components/FeaturedProperties/FeaturedProperties.jsx
+++ src/components/FeaturedProperties/FeaturedProperties.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './Featuredproperties.css';
+import './FeaturedProperties.css';
 import useFetch from '../../hooks/usefetch';
 import { Link, useNavigate } from 'react-router-dom';
 
