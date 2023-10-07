7c2763ec20fa09981baa29d12f7e04e76d04918a
Karthi Keyan <125793976+karthi200125@users.noreply.github.com>
2023-10-07 19:08:52 +0530

    add some style in navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,12 +3,11 @@ import { Link } from 'react-router-dom'
 import { useContext } from 'react';
 import { AuthContext } from '../../context/AuthContext';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronCircleDown, faChevronDown, faChevronUp } from '@fortawesome/free-solid-svg-icons';
+import { faBell, faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
 import { useState } from 'react';
 
 const Navbar = () => {
   const { user, logout } = useContext(AuthContext);
-  const [openuser, setopenuser] = useState(false)
   const handlelogout = () => {
     logout()
     alert("do you want logout")
@@ -19,18 +18,9 @@ const Navbar = () => {
       <div className="navcontainer">
         <Link to="/home" style={{ color: 'inherit', textDecoration: 'none' }}><span className="logo">TravelTrove</span></Link>
         {user ?
-          (<div className="user">
-            <div className="one">
-              <span>{user.username}</span>
-              {openuser ?
-                <FontAwesomeIcon icon={faChevronDown} className='usericon' onClick={() => setopenuser(!openuser)} />
-                :
-                <FontAwesomeIcon icon={faChevronUp} className='usericon' onClick={() => setopenuser(!openuser)} />
-              }
-            </div>
-            {!openuser &&
-              <button onClick={handlelogout}>Logout</button>
-            }
+          (<div className="user">            
+              <span>{user.username}</span>                            
+              <div className='iconhovercon'><FontAwesomeIcon icon={faSignOutAlt} style={{color:'red'}} onClick={handlelogout} /><span className='iconname'>Logout</span></div>                          
           </div>)
           :
           (<div className="navItems">
